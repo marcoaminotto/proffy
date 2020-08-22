5ce12b992cce5baebd04f7a517a21290236631bc
Marco Echevestre <marcoaminotto@hotmail.com>
2020-08-22 10:02:49 +0200

    Import global styles and clean code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
+
+import "./assets/styles/global.css";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
