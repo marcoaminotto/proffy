dc7603b7a298984765c39aae70a5099a9678f270
Marco Echevestre <marcoaminotto@hotmail.com>
2020-08-25 10:30:03 +0200

    Implement the component PageHeader  with a search form of teachers to the TeacherList page, plus add the css of this form for mobile

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
 function TeacherList() {
-  return <h1>Teacher List</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="These are the availables teachers">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Subject</label>
+            <input type="text" id="subject"/>
+          </div>
+          <div className="input-block">
+            <label htmlFor="weekday">Weekday</label>
+            <input type="text" id="weekday"/>
+          </div>
+          <div className="input-block">
+            <label htmlFor="time">Time</label>
+            <input type="text" id="time"/>
+          </div>
+        </form>
+      </PageHeader>
+    </div>
+  );
 }
 
 export default TeacherList;
