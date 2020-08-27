7c3231fcbe0b62bcf84f8e08eb2a175f96554331
Marco Echevestre <marcoaminotto@hotmail.com>
2020-08-27 09:53:07 +0200

    Create structure of TeacherForm page and implement the css for web and for mobile sizes

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
+import './styles.css';
+
 function TeacherForm() {
-  return <h1>Teacher Form</h1>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Awesome that you want to give classes." />
+      <main>
+        <TeacherItem />
+      </main>
+    </div>
+  );
 }
 
 export default TeacherForm;
