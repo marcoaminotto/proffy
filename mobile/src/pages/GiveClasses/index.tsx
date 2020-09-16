63a31c3120aaa9787e79c9dc0eb2deb393fe8c7a
Marco Echevestre <marcoaminotto@hotmail.com>
2020-09-16 17:48:44 +0200

    Create GiveClasses page

--- mobile/src/pages/GiveClasses/index.tsx
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/core';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+
+import styles from './styles';
 
 function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
   return (
-    <View>
-      <Text>Test</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="contain"
+        source={giveClassesBgImage}
+        style={styles.content}
+      >
+        <Text style={styles.title}>Do you want to be a Proffy?</Text>
+        <Text style={styles.description}>
+          To became one, you need to register on our web platform.
+        </Text>
+      </ImageBackground>
+
+      <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+        <Text style={styles.okButtonText}>Got it!</Text>
+      </RectButton>
     </View>
   );
 }
