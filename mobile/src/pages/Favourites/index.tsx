6540b46a80fb1ceae4304a5d97b3db9018042003
Marco Echevestre <marcoaminotto@hotmail.com>
2020-09-16 19:16:16 +0200

    Create PageHeader component and add to Favourite and TeacherList pages

--- mobile/src/pages/Favourites/index.tsx
+++ mobile/src/pages/Favourites/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import styles from "./styles";
+import PageHeader from '../../components/PageHeader';
+
+import styles from './styles';
 
 function Favourites() {
-  return (<View style={styles.container}>
-
-  </View>);
+  return (
+    <View style={styles.container}>
+      <PageHeader title="My Favourite Proffys" />
+    </View>
+  );
 }
 
 export default Favourites;
