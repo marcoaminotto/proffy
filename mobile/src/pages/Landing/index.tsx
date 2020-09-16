56f8bc8664942676e41fad834524f9d7790bae02
Marco Echevestre <marcoaminotto@hotmail.com>
2020-09-16 12:57:04 +0200

    Change buttons for RectButton from gesture-handler

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler'
 
 import styles from './styles';
 
@@ -25,17 +26,17 @@ function Landing() {
       </Text>
 
       <View style={styles.buttonsContainer}>
-        <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+        <RectButton style={[styles.button, styles.buttonPrimary]}>
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Study</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
+        </RectButton>
+        <RectButton
           onPress={handleNavigateToGiveClassesPage}
           style={[styles.button, styles.buttonSecondary]}
         >
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>Give classes</Text>
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
       <Text style={styles.totalConnections}>
