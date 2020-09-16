61bf3f5e88518089b2804a19244f734ec0d77c8d
Marco Echevestre <marcoaminotto@hotmail.com>
2020-09-16 12:51:15 +0200

    Create GiveClasses page and link it to Landing page

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function GiveClasses() {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+}
+
+export default GiveClasses;
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
@@ -8,7 +9,13 @@ import studyIcon from '../../assets/images/icons/study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
-function Landing() {  
+function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses');
+  }
+
   return (
     <View style={styles.container}>
       <Image source={landingImg} style={styles.banner} />
@@ -22,7 +29,10 @@ function Landing() {
           <Image source={studyIcon} />
           <Text style={styles.buttonText}>Study</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+        <TouchableOpacity
+          onPress={handleNavigateToGiveClassesPage}
+          style={[styles.button, styles.buttonSecondary]}
+        >
           <Image source={giveClassesIcon} />
           <Text style={styles.buttonText}>Give classes</Text>
         </TouchableOpacity>
